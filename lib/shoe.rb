df046746fa8de6dd18f1a61d2e1e3d850ddecd1d
Timothy Kiyeng <timothy.kiyeng@student.moringaschool.com>
2022-10-20 13:28:39 +0300

    Completed

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,36 @@
-# Make your shoe class here!
+# class Shoe
+#   attr_accessor :brand
+#   attr_accessor :properties
+#   def initialize(brand)
+#     @brand=brand
+#   end
+#   def properties=(color, size, material, condition)
+#     @color=color
+#     @size=size
+#     @material=material
+#     @condition=condition
+#   end
+# end
+
+class Shoe
+
+	attr_reader :brand
+	attr_accessor :color, :size, :material, :condition
+
+	def initialize(brand)
+		@brand = brand
+	end
+
+	def cobble
+		@condition = "new"
+		puts("Your shoe is as good as new!")
+
+	end
+end
+
+newShoe = Shoe.new("Nike")
+puts newShoe.brand
+
+
+
+
